e6ebcc57b28505cd8fb89fac6df4f89901ba6ff2
Faradaii <kdkfrdy@gmail.com>
2025-07-24 21:11:26 +0800

    feat: add middleware  and update table

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,17 @@
 
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -18,5 +20,15 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('products', ProductController::class);
+Route::middleware(['auth', 'role:admin'])->group(function(){
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
+    Route::resource('products', ProductController::class);
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+});
+
+Route::middleware(['auth', 'role:user'])->group(function(){
+    Route::get('/dashboard', [UserController::class, 'index'])->name('dashboard');
+});
+
 require __DIR__.'/auth.php';
